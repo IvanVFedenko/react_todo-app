3773038706c8001796e57141ca5d96e86ee3aa4b
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-08-01 19:53:29 +0300

    Updates

---
+++ precache-manifest.45bf53125da5686579bbeffbc1232c62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ce995b26f14f745deb1b1bc7f59b54",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "52b723aeeaef8f2128bd",
+    "url": "/react_todo-app/static/css/main.5d1de334.chunk.css"
+  },
+  {
+    "revision": "8b6498524d2785fb1509",
+    "url": "/react_todo-app/static/js/2.f2b4a267.chunk.js"
+  },
+  {
+    "revision": "52b723aeeaef8f2128bd",
+    "url": "/react_todo-app/static/js/main.f481beee.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
